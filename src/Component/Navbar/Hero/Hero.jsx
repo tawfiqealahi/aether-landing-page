50b86149275ffd38c0e2f6197e8bd253bf7c8702
tawfiq <tawfiqealahi99@gmail.com>
2024-11-01 11:10:46 +0600

    icon add

--- src/Component/Navbar/Hero/Hero.jsx
+++ src/Component/Navbar/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import './Hero.css'
 import landingImage from '../../../assets/hero landing page.png'
+import { RiAppleLine } from "react-icons/ri";
+import { IoLogoGooglePlaystore } from 'react-icons/io5';
+
+
 
 const Hero = () => {
     return (
@@ -10,9 +14,13 @@ const Hero = () => {
             <button className='p-2 my-10  font-semibold  heroSignUpButton'>Sign up for free</button>
             <div>
                 <h4 className=''>Or download the app:</h4>
-                <div className='flex justify-between w-60 my-3'>
-                <button className='font-bold text-gray-100 bg-black px-2 p-1' type="button">Apple Store</button>
-                <button className='font-bold text-gray-100 bg-black px-2 p-1' type="button">Apple Store</button>
+                <div className='flex justify-between w-72 my-3'>
+                <button className=' font- text-gray-100 bg-black px-2 p-1' type="button"><RiAppleLine className='w-10' /> <div>
+                <p className=' font-normal text-sm '>Available on the</p> Apple Store
+                </div> </button>
+                <button className='font-bold text-gray-100 bg-black px-2 p-1' type="button">
+                <IoLogoGooglePlaystore />
+                Play store</button>
                 </div>
             </div>
             </div>
